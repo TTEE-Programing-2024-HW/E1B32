54ddf7fbd7bddad410e03bb974b9fbf1defa958a
choco1225 <u11202232@o365.ttu.edu.tw>
2024-05-03 21:15:51 +0800

    第二題原只有個人風格圖示 將後續三到五題打上 我使用switch去選擇a,b,c case a和case A 是直角三角形 輸入第一個選項a以後 要輸入a~n 如果是這範圍以外的字母則錯誤 b和c是變數 起始的b是'a'所儲存的字母 後面b會慢慢減小直到小於或等於a c設置與b相同的值 c會增大到大於或等於a 所以會形成如 c bc abc 直角三角形 case b和case B是99乘法表 輸入第一個選項b以後 d是1到9 e若是2 則先從1開始跑到2 f同理 在這for迴圈去做循環 輸出e*f case c和case C是結束 輸入第一個選項c以後 會問要不要結束 如果我輸入Y/y則繼續 相反輸入N/n則結束

--- HW2.c
+++ HW2.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
+#include <conio.h>
+int main(void){
     printf("-----------\n");
+    printf("<  E1B32  >\n");
+    printf("<   黃    >\n");
+    printf("<   韋    >\n");
+    printf("<   捷    >\n");
+    printf("<         >\n");
+    printf("<         >\n");
+    printf("<         >\n");
     printf("<   請    >\n");
     printf("<   輸    >\n");
     printf("<   入    >\n");
@@ -15,14 +23,9 @@ int main(){
     printf("<         >\n");
     printf("<         >\n");
     printf("<         >\n");
-    printf("<         >\n");
-    printf("<         >\n");
-    printf("<         >\n");
-    printf("<         >\n");
-    printf("<         >\n");
-    printf("<         >\n");
     printf("-----------\n");//個人風格
-    int n,p;
+    int n=0,p,d,e,f;//n是輸入密碼次數，p是密碼，d是九九乘法表的上限，e是外部循環計數器，f是內部循環計數器
+    char ch,a,b,c,y;
 	do
 	{
 		printf("請輸入密碼");
@@ -34,13 +37,69 @@ int main(){
 			printf("\a");
 			break;
 		}
-	}while(p!=2024);//第一題
+	}while(p!=2024);//第一題p若不是2024則繼續迴圈
     system("clear");//清空控制台窗口的內容
-    
-    printf("------------\n");
-    printf("<直角三角形>\n");
-    printf("< 99乘法表 >\n");
-    printf("<   結束   >\n");
-    printf("------------\n");
-	
+    do{
+    printf("--------------\n");
+    printf("<a.直角三角形>\n");
+    printf("<b. 99乘法表 >\n");
+    printf("<c.   結束   >\n");
+    printf("--------------\n");//第二題，輸入a,b,c
+    fflush(stdin);//清除緩存
+    scanf(" %c", &ch);
+    switch(ch) {
+        case 'A':
+        case 'a':
+            printf("輸入一個a~n的字母: ");//a~n
+            fflush(stdin);
+            scanf(" %c", &a);
+            if(a < 'a' || a > 'n') {
+                printf("錯誤,輸入一個a~n的字母\n");
+            }else {
+                for( b = a; b >= 'a'; b--) {
+                    for( c = b; c <= a; c++) {
+                        printf("%c", c);
+                    }
+                    printf("\n");
+                }
+            }
+            break;//第三題
+
+        case 'B':
+        case 'b':
+            printf("請輸入一個1~9的數字: ");//1~9
+            scanf("%d", &d);// 使用者輸入的九九乘法表上限
+            if(d >= 1 && d <= 9) {
+                for(e = 1; e <= d; e++) {// 外部循環，控制行數
+                    for(f = 1; f <= e; f++) {// 內部循環，控制列數
+                        printf("%d ", e * f);
+                    }
+                    printf("\n");
+                }
+            } else {
+                printf("輸入錯誤\n");
+            }
+            break;//第四題
+
+        case 'C':
+        case 'c':
+            printf("Continue? (Y/N): ");
+            scanf(" %c", &y);
+            if(y == 'n' || y == 'N') {
+                printf("夾束\n");
+                return 0;
+            } else if (y == 'y' || y == 'Y') {
+                break;
+            } else {
+                printf("請輸入Y或N\n");//Y/y or N/n
+            }
+            break;//第五題
+
+        default:
+            printf("請輸入有效的選擇 (A/B/C)\n");//若是除了ABC以外的輸入則輸出這行
+            break;
+    }
+    } while(1);
+
+    return 0;
 }
